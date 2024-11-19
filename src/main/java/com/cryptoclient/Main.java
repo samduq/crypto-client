173172b2b7bb7e90a8537cc8a4662dc0593ede7b
qgarot <quentingarot06@gmail.com>
2024-11-19 19:08:12 +0100

    Update main: delete test section

--- src/main/java/com/cryptoclient/Main.java
+++ src/main/java/com/cryptoclient/Main.java
@@ -1,19 +1,9 @@
 package com.cryptoclient;
 
-import com.cryptoclient.application.Application;
 import com.cryptoclient.client.Client;
-import com.cryptoclient.config.Configuration;
 
 public class Main {
     public static void main(String[] args) {
         new Client();
-
-        //testApp();
-    }
-
-    public static void testApp() {
-        Application app = new Application(Configuration.WINDOW_TITLE, Configuration.WINDOW_WIDTH, Configuration.WINDOW_HEIGHT, Configuration.WINDOW_RESIZABLE);
-        app.getViewManager().displayView(Configuration.VIEW_LOGIN);
-        app.setVisible(true);
     }
 }
